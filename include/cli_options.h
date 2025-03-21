e866c9df9b4700e8b1b5c2a83c9f18b1c98c36c3
Legendaryious <magspamnichtsehr@hotmail.com>
2025-03-21 15:29:15 +0100

    Update cli_options.h

--- include/cli_options.h
+++ include/cli_options.h
@@ -1,14 +1,17 @@
 #ifndef CLI_OPTIONS_H
 #define CLI_OPTIONS_H
 
+// Enum for command-line modes
+typedef enum {
+    MODE_NONE = 0,
+    MODE_MAIN = 1 << 0, // 1
+    MODE_CLIENT = 1 << 1  // 2
+} CLIMode;
+
 // Function to display usage information
 void display_usage(const char *program_name);
 
 // Function to handle command-line options
-void handle_cli_options(int argc, char *argv[]);
-
-// Function prototypes for writer and reader
-void writer();
-void reader();
+CLIMode handle_cli_options(int argc, char *argv[]);
 
 #endif // CLI_OPTIONS_H
